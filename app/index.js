5a34692d01366c4728a32feeb56e68556ff4fc22
rbk <bk.ramzi@gmail.com>
2020-02-02 15:12:05 +0100

    create the blockchain network

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -16,8 +18,9 @@ app.get('/blocks', (req, res) => {
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added : ${block.toString()}`);
-
+    p2pServer.synchChains();
     res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
+p2pServer.listen();
